31528275fd84c02733479faaafab83f1796ae40e
bmabe <bmabe002@odu.edu>
2019-09-09 16:37:18 -0400

    Server now echoes until disconnect signal

--- echo_server.rb
+++ echo_server.rb
@@ -2,19 +2,6 @@
 
 require "socket"
 
-#Reads in input from gets until 
-def gets_multiline(client)
-        text = ''
-        line = 'a'
-
-        while line != '' do
-                line = client.gets.chomp
-                text += line + "\n" unless line.empty?
-        end
-
-        return text
-end
-
 host = "0.0.0.0"  #host is hardcoded for now; will switch to arg
 port = 80	  #default port of 80, overidden by 1st argument
 
@@ -26,15 +13,19 @@ puts "Listening on #{host}:#{port} ..."
 loop do
 	Thread.start(socket.accept) do |client|
 		puts "connected to #{client}"
-		client.write("This is an echo server!\nI repeat what you say")
-		client.write("\n...until the first empty line that is...\n")
-		
-		message = gets_multiline(client)
-		puts "#{client} sez: " + message
-		client.write message
-		
-		puts "disconnecting #{client}, goodbye!"
+		client.write("This is an echo server!\nI repeat each line\n")
+		client.write("until you disconnect\n")
+		message = 'a'
 
-		client.close
+		while true do
+			message = client.gets("\n")
+			
+			unless message.nil?
+				puts "#{client} sez: " + message
+				client.write "> " + message 
+			end
+		end
+
+		puts "disconnected from #{client}"		
 	end
 end
