06a0f1c470f47e2e5ba17a86add4d637bfd97af2
bmabe <bmabe002@odu.edu>
2019-09-25 15:51:14 -0400

    more progress on validating requests; added responses.rb

--- src/req-res.rb
+++ src/req-res.rb
@@ -1,12 +1,79 @@
+require_relative "responses"
+
 class Request
-	def initialize()
-		@method = ""
-		@uri = ""
-		@version = ""
-		@headers = Hash.new	# Empty by default
-		@valid = false		# Initialzed to false	 
+	# @param the whole request as a multi-line string
+	#  or nothing at all
+	def initialize(req)
+		if req != nil
+			unless req.respond_to? :split?
+				raise ArgumentError "must be string"
+			end
+
+			@directive = ""
+			@method = ""
+			@uri = ""
+			@version = ""
+			@headers = nil
+			@valid = false
+			
+
+			# Split request message into lines
+			if req.includes?("\r")
+				lines = req.split("\r\n")
+			else
+				lines = req.split("\n")
+			end
+
+			if lines.length > 1
+				#main branch
+				@directive = lines[0].split(' ')
+				
+				if @directive.length == 3
+					@method = @directive[0]
+					@uri = @directive[1]
+					@version = @directive[2]
+					
+					# inputs headers
+					# headerLines = lines[1...lines.length]] 
+					# @headers = headerLines.split(":")
+					headerLines = lines[1...lines.length]
+					headerLines.each { |i| j = i.split(":"); @headers[j[0]] = j[1] }
+					
+					# Was going to process empty headers here, but RFC 7231 allows this
+				
+					# Extract host for URI validation
+					host = @headers.each { |i,j| return j if i == "Host"}
+
+					#validate method
+					#validate uri
+					#validate version
+				else
+					@responseCode = 401 # Bad request
+				end
+			else
+				@responseCode = 401 # Bad request
+			end
+			
+			
+		else
+			@directive = ""
+			@method = ""
+			@uri = ""
+			@version = ""
+			@headers = Hash.new	# Empty by default
+			@valid = false		# Initialzed to false
+			@responseCode = 200	# Defaults to 200 OK
+		end
 	end
+
+	def responseCode()
+		return @responseCode.to_s() + " " + RESPONSES[@responseCode] + "\r\n"
+	end
+	
+	attr_reader :valid, :uri, :headers, :method
+
 	def parse(reqString)
+		tempArr =  @directive.split(' ')
 		
 	end
 end
---
+++ src/responses.rb
@@ -0,0 +1,11 @@
+RESPONSES = 
+	{
+		
+		200 => "OK",
+		400 => "Bad Request",
+		403 => "Forbidden",
+		404 => "Not Found",
+		500 => "Internal Server Error",
+		501 => "Not Implemented",
+		505 => "HTTP Version Not Supported"
+	}
