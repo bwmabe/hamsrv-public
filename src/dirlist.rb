14a5979d526af4aac6603410645dcc30412d11c3
bmabe <bmabe002@odu.edu>
2019-10-07 13:04:06 -0400

    stub for 'index.html'-less dir listing

--- src/dirlist.rb
+++ src/dirlist.rb
@@ -1,5 +1,25 @@
-list = Dir.entries(loc)
-files = list.select{|f| !File.directory? f}
-dirs =  list.select{|f| File.directory? f}
+def genDirListing(fname, webroot)
+	begin
+		if File.directory?(fname)
+			list = Dir.entries(fname)
+		else
+			list = Dir.entries(fname.realdirpath.sub(fname,""))
+		end
+		
+		list.split!(webroot)[-1]
 
-html = "<!DOCTYPE html>\n<html><head><title>Index of $LOC</title></head>\n<body><h1></h1></body></html>"
+		files = list.select{|f| !File.directory? f}
+		dirs =  list.select{|f| File.directory? f}
+	rescue
+		#return 501 error page
+	end
+
+	html-start = "<!DOCTYPE html>\n<html><head><title>Index of $LOC</title></head>\n<body><h1></h1>"
+	html_end = "</body></html>"
+
+	link_start = "<a href=\"$PATH\">$FNAME<a>"
+
+	# line format: link_start\tsize\tmodified
+
+	html_start.sub!("$LOC", list)
+end
