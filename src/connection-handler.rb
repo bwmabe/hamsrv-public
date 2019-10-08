4774c6f7a041506c9bdc9a7738e6bd8b2c14cf01
Ben Mabe <bmabe002@odu.edu>
2019-10-08 19:24:52 -0400

    possibly finished configurable web root, shelving for now...

--- src/connection-handler.rb
+++ src/connection-handler.rb
@@ -5,8 +5,17 @@ require_relative "config-loader"
 require_relative "req-res"
 require_relative "eval-request"
 
+def isBlank?( *x )
+	begin
+		return x[0].empty?
+	rescue 
+		return x[0].nil?
+	end
+end
+
 def handleConnection(client,config)
-	puts "connected to #{client.peeraddr[-1]}:#{config["port"]}"
+	ip = client.peeraddr[-1]
+	puts "connected to #{ip}:#{config["port"]}"
 	
 	#message = ''
 	close = false
@@ -28,12 +37,16 @@ def handleConnection(client,config)
 		# If the message is not empty; process the request form the client
 		unless request.empty?
 			# request, response, client ip, config file
-			evalReq(Request.new(request), response, client.peeraddr[-1],config)
-			
+			unless isBlank?( config["web-root"] )
+				evalReq(Request.new(request, config["web-root"]), response, ip, config)
+			else
+				evalReq(Request.new(request), response, ip, config)
+			end
+
 			# Send the response
 			client.write response.print
 		end
 	end
 
-	puts "disconnected from #{client.peeraddr[-1]}:#{config["port"]}"
+	puts "disconnected from #{ip}:#{config["port"]}"
 end
