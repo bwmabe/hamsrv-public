4c40256b3a7721c8d690aafd37670e3d3907bef4
bub <benbub1@gmail.com>
2019-10-09 16:02:07 -0400

    Fixing bugs

--- src/time-date.rb
+++ src/time-date.rb
@@ -39,35 +39,38 @@ end
 def newer?(t1,t2)
 	t1regex = /(Mon|Tue|Wed|Thu|Fri|Sat|Sun), \d\d (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d\d\d\d \d\d:\d\d:\d\d GMT/.match(t1)
 	t2regex =  /(Mon|Tue|Wed|Thu|Fri|Sat|Sun), \d\d (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d\d\d\d [0-2][0-9]:[0-5][0-9]:[0-5][0-9] GMT/.match(t2)
+	begin
+		if !t2regex.nil? || !t1regex.nil?
+			
+			t1d, t1month, t1y, t1time = t1regex[0].split[1..4]
+			t2d, t2month, t2y, t2time = t2regex[0].split[1..4]
 
-	if !t2regex.nil? || !t1regex.nil?
-		
-		t1d, t1month, t1y, t1time = t1regex[0].split[1..4]
-		t2d, t2month, t2y, t2time = t2regex[0].split[1..4]
+			# Compare years
+			return false if t1y.to_i < t2y.to_i
+			return true if t1y.to_i > t2y.to_i
 
-		# Compare years
-		return false if t1y.to_i < t2y.to_i
-		return true if t1y.to_i > t2y.to_i
-
-		# Compare months
-		return false if MONTHS[t1month] < MONTHS[t2month]
-		return true if MONTHS[t1month] > MONTHS[t2month]
+			# Compare months
+			return false if MONTHS[t1month] < MONTHS[t2month]
+			return true if MONTHS[t1month] > MONTHS[t2month]
 		
-		# Compare Days
-		return false if t1d.to_i < t2d.to_i
-		return true if t1d.to_i > t1d.to_i
+			# Compare Days
+			return false if t1d.to_i < t2d.to_i
+			return true if t1d.to_i > t1d.to_i
 		
-		# Compare time
-		t1h, t1m, t1s = t1time.split(":").map{|i| i.to_i}
-		t2h, t2m, t2s = t2time.split(":").map{|i| i.to_i}
-		return false if t1h < t2h
-		return true if t1h > t2h
+			# Compare time
+			t1h, t1m, t1s = t1time.split(":").map{|i| i.to_i}
+			t2h, t2m, t2s = t2time.split(":").map{|i| i.to_i}
+			return false if t1h < t2h
+			return true if t1h > t2h
 	
-		return false if t1m < t2m
-		return true if t1m > t2m
+			return false if t1m < t2m
+			return true if t1m > t2m
 		
-		return false if t1s < t2s
+			return false if t1s < t2s
 
+		end
+	rescue
+		return true
 	end
 
 	# If the times can't be compared, just ignore and return true
